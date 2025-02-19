2d3ccde3faeca1aacfad0b2858c38141b15e9d9f
classuser60 <classuser60@local.iful.edu.ua>
2025-02-19 11:07:15 +0200

    Add my line

--- first_task.py
+++ first_task.py
@@ -1,2 +1,5 @@
 string_1 = 'Hi, my name is Kosmii Yurii and I’m studying software development in college'
 print(string_1)
+
+string_2 = 'Hi, my name is Tsiupiak Iryna and I’m studying software development in college'
+print(string_2)
